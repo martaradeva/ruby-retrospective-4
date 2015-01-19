166505e97b85ca15cb62533a81a95701868b46e3
Martina Radeva <martaradeva@yahoo.com>
2015-01-19 16:55:51 +0200

    task 03 WIP

--- solutions/03.rb
+++ solutions/03.rb
@@ -16,25 +16,25 @@ module RBFS
 
     private
 
-    def self.parse_all_objects(klass)#klass is RBFS::File or RBFS::Directory
+    def self.parse_all_objects(class_name)
       parsed_objects = {}
       number_of_objects = next!.to_i
       number_of_objects.times do
-        parsed_objects.merge! parse_single_object(klass)
+        parsed_objects.merge! parse_single_object(class_name)
       end
       parsed_objects
     end
 
-    def self.parse_single_object(klass)
+    def self.parse_single_object(class_name)
       name = next!
       length = next!
       serialized = next!(length)
-      parsed = klass.parse(serialized)
+      parsed = class_name.parse(serialized)
       {name => parsed}
     end
 
     def self.next! (*piece_length)
-      if piece_length.length > 0 
+      if piece_length.length > 0
         then @string_to_parse.slice! (0..piece_length[0].to_i-1)
         else chunk, @string_to_parse = @string_to_parse.split(":", 2)
         chunk
@@ -51,11 +51,11 @@ module RBFS
 
     def data_type
       case @data
-        when String                    then @data_type = :string
-        when Symbol                    then @data_type = :symbol
-        when Numeric                   then @data_type = :number
-        when (TrueClass or FalseClass) then @data_type = :boolean
-        else                                @data_type = :nil
+        when nil     then @data_type = :nil
+        when String  then @data_type = :string
+        when Symbol  then @data_type = :symbol
+        when Numeric then @data_type = :number
+        else              @data_type = :boolean
       end
     end
 
@@ -66,8 +66,6 @@ module RBFS
     def self.parse(string)
       data_type, data_string = string.split(":", 2)
       file_content = parse_data(data_type, data_string)
-      # file_content = parse_data *string.split(":", 2) # I don't really understand what the wilcard goes for
-      # http://fmi.ruby.bg/lectures/05-blocks-procs-parallel-assignment-classes-enumerable#41
       File.new (file_content)
     end
 
@@ -77,9 +75,13 @@ module RBFS
         when "string" then parsed = data
         when "symbol" then parsed = data.to_sym
         when "number" then parsed = parse_number(data)
-        when "boolean" then (data == "true") ? parsed = true : parsed = false
-        when "" then parsed = nil
+        when "nil"    then parsed = nil
+        else               parsed = 'true'
       end
+    end
+
+    def self.parse_boolean(data)
+      if data then true else false end
     end
 
     def self.parse_number(data)
@@ -102,10 +104,10 @@ module RBFS
 
     def add_directory(name, *directory)
       if directory.length > 0
-      then new_dir = {name => directory[0]}
-      else new_dir = {name => RBFS::Directory.new}
+      then new_directory = {name => directory[0]}
+      else new_directory = {name => RBFS::Directory.new}
       end
-      @directories.merge!(new_dir)
+      @directories.merge!(new_directory)
     end
 
     def [](key)
@@ -113,22 +115,21 @@ module RBFS
     end
 
     def serialize
-      result = ''
+      result = []
       result << @files.length.to_s + ":"
-      @files.each do |name, file| 
+      @files.each do |name, file|
         result << [name, file.serialize.length.to_s, file.serialize].join(":")
       end
       result << @directories.length.to_s + ":"
       @directories.each do |name, directory|
-        result << name << ":"
-        result << directory.serialize
+        result << name << ":" << directory.serialize
       end
-      result
+      result.join ""
     end
 
     def self.parse(string)
       RBFS::Parser.parse_directory(string)
     end
   end
-  
+
 end
